ae57ed8ada56df16454b402ef857f19ed1ff8bce
Airwoman <airwomanzhy@hotmail.com>
2016-05-02 01:16:46 +1000

    add wheel of fortune

--- HarrisonChow/week_01/MTA/MTA.js
+++ HarrisonChow/week_01/MTA/MTA.js
@@ -1,3 +1,5 @@
+
+//define the line and stops by objects and arrays
 var lineN = ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"];
 var lineL = ["8th", "6th", "Union Square", "3rd", "1st"];
 var line6 = ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"];
@@ -6,51 +8,49 @@ var lineList = {
   "L": lineL,
   "6": line6
 };
+// all messages will be used for console.
 var tripMessageOne = "Your must travel through the following stops on the ";
 var tripMessageTwo = "Change at Union Square.";
 var tripMessageThree = "Your journey continues through the following stops: ";
 var tripMessageFour = " stops in total.";
 
+// this function will list the all stops from departure station to destination(transfer station)
+var stopList = function(from, to, line) {
+  if (from < to){
+    trip = line.slice(from+1, to+1).toString();
+  } else {
+    trip = line.slice(to, from).reverse().toString();
+  }
+  return trip;
+}
+//this function is for the departure station and  destination are in the same line.
 var sameLine = function (fromLine, startStation, stopStation) {
   var startLine = lineList[fromLine];
   var indexOfStartStation = startLine.indexOf(startStation);
   var indexOfStopStation = startLine.indexOf(stopStation);
   var totalStops = Math.abs(indexOfStopStation - indexOfStartStation);
-  var followStops;
-  if (indexOfStartStation < indexOfStopStation){
-    followStops = startLine.slice(indexOfStartStation+1, indexOfStopStation+1).toString();
-  } else {
-    followStops = startLine.slice(indexOfStopStation, indexOfStartStation).reverse().toString();
-  }
+
+  var followStops = stopList(indexOfStartStation, indexOfStopStation, startLine)
   var message = tripMessageOne + fromLine +" line: " + followStops + "\n" + totalStops + tripMessageFour;
   console.log(message);
 }
 
+//this function will consider about the transfer station.
 var differentLine = function (fromLine, startStation, toLine, stopStation) {
   var startLine = lineList[fromLine];
   var stopLine = lineList[toLine];
-  var followStopsOne;
-  var followStopsTwo;
   var indexOfStartStation = startLine.indexOf(startStation);
   var indexOfStopStation = stopLine.indexOf(stopStation);
   var indexOfTransferOnStart = startLine.indexOf("Union Square");
   var indexOfTransferOnStop = stopLine.indexOf("Union Square");
-  if (indexOfStartStation < indexOfTransferOnStart){
-    followStopsOne = startLine.slice(indexOfStartStation+1, indexOfTransferOnStart+1).toString();
-  } else {
-    followStopsOne = startLine.slice(indexOfTransferOnStart, indexOfStartStation).reverse().toString();
-  }
-  if (indexOfTransferOnStop < indexOfStopStation){
-    followStopsTwo = stopLine.slice(indexOfTransferOnStop+1, indexOfStopStation+1).toString();
-  } else {
-    followStopsTwo = stopLine.slice(indexOfStopStation, indexOfTransferOnStop).reverse().toString();
-  }
+  var followStopsOne = stopList(indexOfStartStation, indexOfTransferOnStart, startLine);
+  var followStopsTwo = stopList(indexOfTransferOnStop, indexOfStopStation, stopLine);
   var totalStops = Math.abs(indexOfTransferOnStart - indexOfStartStation) + Math.abs(indexOfStopStation - indexOfTransferOnStop);
   var message = tripMessageOne + fromLine +" line: " + followStopsOne + "\n" +
       tripMessageTwo + "\n" + tripMessageThree + followStopsTwo + "\n" + totalStops + tripMessageFour;
   console.log(message);
 }
-
+// The MTA main function which is combined the two situations.
 var PlanTrip = function (fromLine, startStation, toLine, stopStation) {
   if (fromLine === toLine) {
     sameLine(fromLine, startStation, stopStation);
--- HarrisonChow/week_01/TheWordGuesser/wheelOfFortune.js
+++ HarrisonChow/week_01/TheWordGuesser/wheelOfFortune.js
@@ -1,8 +1,12 @@
-// Homework: The Word Guesser
+// Bonus: Make it more like Wheel of Fortune:
+//
+// Start with a reward amount of $0
+// Every time a letter is guessed, generate a random amount and reward the user if they found a letter (multiplying the reward if multiple letters found), otherwise subtract from their reward.
+// When they guess the word, log their final reward amount.
 
-// Create two global arrays: one to hold the letters of the word (e.g. 'F', 'O', 'X'), and one to hold the current guessed letters (e.g. it would start with '', '', '_' and end with 'F', 'O', 'X').
 var letterTarget = ["A", "P", "P", "L", "E"];
 var letterGuessed = ["_", "_", "_", "_", "_"];
+var reward = 0;
 //  function of guessLetter
 var guessedLetters = function () {
   // console.log("Please start the word gussing.");
@@ -13,15 +17,20 @@ var guessedLetters = function () {
       if (input === letterTarget[i]) {
         letterGuessed[i] = letterTarget[i];  // align the correct value to letterGuessed array
         bingo = true;
+
       }
     }
     if(bingo) {
-      alert(letterGuessed.join(" ")); //show the guessed charactor
+      reward += Math.round(Math.random()*100);
+      console.log(reward);
+      alert(letterGuessed.join(" ") + "\n" + "Your reward is $"+ reward); //show the guessed charactor
     } else {
-      alert('Try again');
+      reward -= Math.round(Math.random()*100);
+
+      alert('Try again' + "\n" + "You balace is $" + reward);
     }
   }
-  alert("Congratulation!");
+  alert("Congratulation! \n" + "You earnd $" + reward +" in total." );
 
 
 }
