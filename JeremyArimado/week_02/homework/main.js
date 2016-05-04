4ce4407527bef3a2916eef5cb380e7dbaba00f0f
Jeremy Arimado <jarimado@gmail.com>
2016-05-05 07:47:59 +1000

    [hw] init

---
+++ JeremyArimado/week_02/homework/main.js
@@ -0,0 +1,25 @@
+console.log('i work');
+
+
+var cat = {};
+
+cat.static.img = {
+    scary: 'https://45.media.tumblr.com/6b52bce5723d89b9df196fa9021cb21f/tumblr_nw0lr1Mqmn1ttovh1o1_500.gif'
+};
+
+cat.model.randomScreenPos = function (img) {
+
+    // get random img from static
+    // use those coordinates to get a screen position
+};
+
+cat.render.createElement = function (randomScreenPos, element) {
+    // create element at randomScreenPosition
+};
+
+cat.render.placeElement = function (randomScreenPos, element) {
+    // place element on screen
+}; 
+
+cat.init = function () {
+};
