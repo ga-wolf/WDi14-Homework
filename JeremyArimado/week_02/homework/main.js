5a68f490b4982bebec243cedbec251536ef1ce71
doloreel <jarimado@gmail.com>
2016-05-06 11:26:17 +1000

    finished cats

--- JeremyArimado/week_02/homework/main.js
+++ JeremyArimado/week_02/homework/main.js
@@ -8,15 +8,31 @@ cat.update = {};
 cat.model = {};
 cat.render = {};
 cat.init = {};
+cat.util = {};
+
+// UTILITY Functions
+
+cat.util.getRandomIndex = function ( arr ) {
+    return Math.abs(Math.round(Math.random() * arr.length - 1));
+}
+
+//
 
 cat.static.binCounter = 50;
+
+cat.static.audio = 'hypnotoad.mp3';
+
 cat.static.img = [
-        'http://www.catgifs.org/wp-content/uploads/2013/09/074_evil_cat_gifs.gif', 'https://45.media.tumblr.com/6b52bce5723d89b9df196fa9021cb21f/tumblr_nw0lr1Mqmn1ttovh1o1_500.gif'
-    ];
+    'http://www.catgifs.org/wp-content/uploads/2013/09/074_evil_cat_gifs.gif',
+    'https://45.media.tumblr.com/6b52bce5723d89b9df196fa9021cb21f/tumblr_nw0lr1Mqmn1ttovh1o1_500.gif'
+];
 
 cat.static.words = ['DONALD TRUMP', '2AM LOCKOUTS', 'TURNBULL', 'UNEMPLOYMENT', 'CLICKBAIT', 'STUDENT LOANS', 'SHIT'];
+cat.static.colors = ["#CCCCCC","#00CC00","#000000","#020202","#010101","#0000CC","#CC0000"];
 
 cat.model.objects = [];
+
+// RANDOM CATS
 
 cat.model.createObject = function () {
     return {
@@ -34,7 +50,8 @@ cat.model.randomise = function ( object, imgArray ) {
     var screenWidth = window.innerWidth; //get screenHeight
     var randomX = Math.floor(Math.random() * screenWidth);
     var randomY = Math.floor(Math.random() * screenHeight);
-    var randomImg = imgArray[Math.round(Math.random() * imgArray.length)];
+    var randNum = cat.util.getRandomIndex(imgArray);
+    var randomImg = imgArray[randNum];
     currentCat.x = randomX;
     currentCat.y = randomY;
     currentCat.id = null;
@@ -44,7 +61,6 @@ cat.model.randomise = function ( object, imgArray ) {
 };
 
 cat.update.objects = function (objectsArr) {
-
     for (var i = 0; i < objectsArr.length; i += 1) {
         objectsArr[i].x += 10;
     };
@@ -71,17 +87,35 @@ cat.paint = function () {
     // well these objects have to re intialise themselves
 };
 
-cat.init.step = function () {
+cat.init.randomCats = function () {
+    cat.static.binCounter -= 1;
     var newCat = cat.model.createObject(100, 100, 'ID', 'content');
     var randomisedCat = cat.model.randomise(newCat, cat.static.img);
     var newCatElement = cat.render.createElement(randomisedCat);
-    cat.render.placeElement(newCatElement); 
+    cat.render.placeElement(newCatElement);
 };
+
+cat.init.setRandomBackround = function () {
+    console.log('this should fire multiples')
+    var overlay = document.getElementById('overlay');
+    var line1 = document.getElementsByClassName('line1')[0];
+    var line2 = document.getElementsByClassName('line2')[0];
+    var randomColor = cat.static.colors[cat.util.getRandomIndex(cat.static.colors)];
+    var randomColor2 = cat.static.colors[cat.util.getRandomIndex(cat.static.colors)];
+    var randomColor3 = cat.static.colors[cat.util.getRandomIndex(cat.static.colors)];
+    console.log(randomColor);
+
+    overlay.style.background = randomColor;
+    line1.style.background = randomColor2;
+    line2.style.background = randomColor3;
+
+}
+
 
 cat.init.loop = function () {
-    var init = setInterval(cat.init.step, 300);
+    var randomCats = setInterval(cat.init.randomCats, 300);
+    var randomBG = setInterval(cat.init.setRandomBackround, 10);
 };
-
 
 // setInterval(function () {
 //     // change word;
