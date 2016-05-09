349bf05f1d951dfe4f1500df51a0d122eb0ec335
Airwoman <airwomanzhy@hotmail.com>
2016-05-10 00:48:11 +1000

    basic functions all done

--- HarrisonChow/week_03/project0/main.js
+++ HarrisonChow/week_03/project0/main.js
@@ -1,55 +1,106 @@
-var wonThree = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-];
-var wonFour = [
-  [1,2,3,4],
-  [5,6,7,8],
-  [9,10,11,12],
-  [13,14,15,16],
-  [1,5,9,13],
-  [2,6,10,14],
-  [3,7,11,15],
-  [4,8,12,16],
-  [1,6,11,16],
-  [4,7,10,13]
-];
+var sameRow = function(moves, grid) {
+  var allYs = moves.map(function(value) { return value[1]; });
+  var win = false;
+  for(var row=0; row < grid; row ++) {
+    var matchedRow = allYs.filter(function(value) { return value === row });
+    if(matchedRow.length === grid) {
+      return win = true;
+    }
+  }
+  return win;
+};
+
+var sameColumn = function(moves,grid) {
+  var allXs = moves.map(function(value){ return value[0]; });
+  var win = false;
+  for (var column = 0; column < grid; column++) {
+    var matchedColumn = allXs.filter(function(value) {return value === column;});
+    if (matchedColumn.length === grid){
+      return win = true;
+    }
+  }
+  return win;
+};
+
+
+var diagonalForward = function(moves, grid) {
+  var win = false;
+  var sameXYs = moves.filter(function(value) {
+    return value[0]===value[1];
+  });
+  if(sameXYs.length===grid){
+    return win = true;
+  }
+  return win;
+};
+
+var diagonalBackward = function(moves, grid) {
+  var win = false;
+  var xPlusYs = moves.filter(function(value) {
+    return value[0]+value[1] === grid - 1;
+  });
+  if (xPlusYs.length === grid) {
+    return win = true;
+  }
+  return win;
+};
+
+
+var checkAll = function(moves,grid) {
+  return sameRow(moves, grid) || sameColumn(moves, grid) || diagonalForward(moves,grid) || diagonalBackward(moves, grid);
+}
+
 
 var playerOneCurrent = [];
 var playerTwoCurrent = [];
 
 var playerOne = 1;
 var playerTwo = -1;
+var PlayerOneCount = 0;
+var PlayerTwoCount = 0;
+var tieCount = 0;
 var currentPlayer = playerOne;
 var numGet = 3;
-
-var cells = [];
+var cells= {};
 var createPlayBoard = function () {
   var won=[];
   $("div.main").remove();
-  numGet = $("input[name='games']:checked").val();
+  numGet = parseInt($("input[name='games']:checked").val());
   for (var j = 0; j < numGet; j++) {
     var $newDivOut = $("<div class='main'></div>");
     $newDivOut.appendTo(document.body);
     for (var i = 0; i < numGet; i++) {
       var $newDivIn = $("<div class='cell' id='cell"+(j*numGet+i)+"'></div>");
       $newDivIn.appendTo($newDivOut);
-      cells.push("cell"+(j*numGet+i));
-      // won.push([i,j]);
-      // for (var k = 0; k < won.length; k++) {
-      //   for (var m = 0; m < numGet; m++) {
-      //   console.log(won[k][m]);
-      //   }
-      // }
+      cells["cell"+(j*numGet+i)] = [i, j];
     }
   }
   addEventHandlers();
+}
+
+var playerEvent = function(playerMoves) {
+  var playerName = currentPlayer === playerOne ? 'Player One' : 'Player Two';
+  currentPlayer = currentPlayer * -1;
+  var playerMovesSorted = playerMoves.sort(function(a, b){return a-b});
+  wonThree.forEach(function(win) {
+    var numOfWins = win.filter(function(value) { return playerMovesSorted.indexOf(value) != -1}).length;
+    if(numOfWins === 3) {
+      $("#popupcontent").html("Congratulations!  "+ playerName + " is the winner!");
+      showPopup(400,200);
+      $("div.cell").css({
+        "pointer-events":"none"
+      })
+    }
+  });
+}
+
+var annouceWinner = function() {
+  var playerName = currentPlayer === playerOne ? 'Player One' : 'Player Two';
+  $("#popupcontent").html("Congratulations!  "+ playerName + " is the winner!");
+  showPopup(400,200);
+  $("div.cell").css({
+    "pointer-events":"none"
+  })
 }
 
 var addEventHandlers = function () {
@@ -60,46 +111,30 @@ var addEventHandlers = function () {
 
     if (currentPlayer === playerOne) {
       $currentCell.css("backgroundImage", "url(images/cross.png)");
-      playerOneCurrent.push(cells.indexOf($currentCell.attr('id')));
-      currentPlayer = playerTwo;
-      var playOneCurrentSorted = playerOneCurrent.sort(function(a, b){return a-b});
-      console.log('move:', playOneCurrentSorted);
-      console.log('totalGrid:', numGet);
-      wonThree.forEach(function(win) {
-        var numOfWins = win.filter(function(value) { return playOneCurrentSorted.indexOf(value) != -1}).length;
-        console.log('numberOfWins:', numOfWins);
+      playerOneCurrent.push(cells[$currentCell.attr('id')]);
 
-        if(numOfWins === 3) {
-          $("#popupcontent").html("Congratulations!  Player One is the winner!");
-          showPopup(400,200);
-          $("div.cell").css({
-            "pointer-events":"none"
-          })
-        }
-      });
+      if(checkAll(playerOneCurrent, numGet)) {
+        annouceWinner();
+        PlayerOneCount +=1;
+        console.log("one "+PlayerOneCount);
+      }
 
     } else {
       $currentCell.css("backgroundImage", "url(images/circle.png)");
-      playerTwoCurrent.push(cells.indexOf($currentCell.attr('id')));
-      currentPlayer = playerOne;
-      var playTwoCurrentSorted = playerTwoCurrent.sort(function(a, b){return a-b});
-      console.log('move:', playTwoCurrentSorted);
-      wonThree.forEach(function(win) {
-        var numOfWins = win.filter(function(value) { return playTwoCurrentSorted.indexOf(value) != -1}).length;
-        console.log('numberOfWins:', numOfWins);
+      playerTwoCurrent.push(cells[$currentCell.attr('id')]);
 
-        if(numOfWins === 3) {
-          $("#popupcontent").html("Congratulations!  Player Two is the winner!");
-          showPopup(400,200);
-          $("div.cell").css({
-            "pointer-events":"none"
-          })
-        }
-      });
-
+      if(checkAll(playerTwoCurrent, numGet)) {
+        annouceWinner();
+        PlayerTwoCount +=1;
+        console.log("two "+PlayerTwoCount);
+      }
     }
-
-    console.log('innerHTML: ', $currentCell.attr('id'));
+    if ((playerOneCurrent.length+playerTwoCurrent.length) === numGet*numGet) {
+      alert("Tie!")
+      tieCount +=1;
+      reset();
+    }
+    currentPlayer = currentPlayer * -1;
   })
 }
 
@@ -121,14 +156,15 @@ function showPopup(w,h){
    popUp.style.visibility = "visible";
 }
 
+var reset = function() {
+  createPlayBoard();
+  playerOneCurrent = [];
+  playerTwoCurrent = [];
+  currentPlayer = playerOne;
+}
+
 function hidePopup(){
    var popUp = document.getElementById("popupcontent");
    popUp.style.visibility = "hidden";
-   createPlayBoard();
-   playerOneCurrent = [];
-   playerTwoCurrent = [];
-   won = [];
-   playerOne = 1;
-   playerTwo = -1;
-
+   reset();
 }
