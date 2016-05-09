515039e43f627d663ef584c0c18070b8ee6151f0
danharsanyi <dan@conqr.co>
2016-05-09 15:06:55 +1000

    game except add to array and check winner

--- DanielHarsanyi/week_03/9-5-16/Tic-Tac-Toe/main.js
+++ DanielHarsanyi/week_03/9-5-16/Tic-Tac-Toe/main.js
@@ -1,10 +1,51 @@
-var gameBoard = [[cellOne, cellTwo, cellThree],[cellFour, cellFive, cellSix],[cellSeven, cellEight, cellNine]];
+var gameBoard = [["cellOne", "cellTwo", "cellThree"],["cellFour", "cellFive", "cellSix"],["cellSeven", "cellEight", "cellNine"]];
 
-var showX = function () {
-  //capture which cell was clicked - mapped to 'this'
-  //append 'x' to the correct div
-    console.log("cell was clicked");
+var playX = function ($cell) {
+    $($cell).addClass("xImage");
+    $($cell).unbind("click");
 };
 
+var playO = function ($cell) {
+    $($cell).addClass("oImage");
+    $($cell).unbind("click");
+};
 
-$(".cell").on("click", showX);
+var addToArray = function ($cell, player){
+    var currentCell = $cell.id;
+    if (player === true){
+          console.log(currentCell);
+    } else if (player === false ){
+          console.log(currentCell);
+        }
+    };
+
+var checkWinner = function (){
+    //for loop running through each winning combo
+};
+
+var playersTurn = 0;
+var turnCounter = function (){
+    playersTurn += 1;
+    if (playersTurn % 2 !== 0) {
+      return true;
+  } else {
+      return false;
+  }
+};
+
+var gamePlay = function(){
+    var player = turnCounter();
+    if (player === true) {
+        playX(this);
+        addToArray(this, player);
+        checkWinner();
+        return;
+  } else if (player === false ) {
+        playO(this);
+        addToArray(this, player);
+        checkWinner();
+        return;
+  }
+};
+
+$(".cell").on("click", gamePlay);
