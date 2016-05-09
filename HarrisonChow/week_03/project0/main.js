451c6461b95c245f7c919d77210a3849ed14ed86
Airwoman <airwomanzhy@hotmail.com>
2016-05-09 21:20:29 +1000

    check wins by comparing array instead of string

--- HarrisonChow/week_03/project0/main.js
+++ HarrisonChow/week_03/project0/main.js
@@ -23,158 +23,112 @@ var wonFour = [
 
 var playerOneCurrent = [];
 var playerTwoCurrent = [];
+
 var playerOne = 1;
 var playerTwo = -1;
-var currentPlayer;
-
-// var cells = ['cellOne','cellTwo','cellThree', 'cellFour', 'cellFive', 'cellSix', 'cellSeven', 'cellEight','cellNine'];
-// var cellss =[];
-// var createCells = function (num) {
-//   var x;
-//   var y;
-//   for (var i = 0; i < num*num; i++) {
-//     x = i%num;
-//     y = Math.floor(i/num);
-//     cellss.push([x,y]);
-// }
-// }
-// createCells(3);
-
-
-
-// var won = function (num) {
-//   for (var i = 0; i < cells.length; i++) {
-//     for (var j = 0; j < num; j++) {
-//       if (cells[i][j]) {
-//
-//       }
-//
-//       cells[i][j]
-//     }
-//
-//
-//   }
-//
-//
-// }
+var currentPlayer = playerOne;
+var numGet = 3;
 
 var cells = [];
-var createPlayBoard = function (numGet) {
-  if ($("div.main").length){
-    $("div.main").remove();
-    var numGet = $("input[name='games']:checked"). val();
-    for (var j = 0; j < numGet; j++) {
-      var $newDivOut = $("<div class='main'></div>");
-      $newDivOut.appendTo(document.body);
-      for (var i = 0; i < numGet; i++) {
-        var $newDivIn = $("<div class='cell' id='cell"+(j*numGet+i)+"'></div>");
-        $newDivIn.appendTo($newDivOut);
-        cells.push("cell"+(j*numGet+i));
-      }
-    }
-    addEventHandlers();
-  } else {
-    var numGet = $("input[name='games']:checked"). val();
-    for (var j = 0; j < numGet; j++) {
-      var $newDivOut = $("<div class='main'></div>");
-      $newDivOut.appendTo(document.body);
-      for (var i = 0; i < numGet; i++) {
-        var $newDivIn = $("<div class='cell' id='cell"+(j*numGet+i)+"'></div>");
-        $newDivIn.appendTo($newDivOut);
-        cells.push("cell"+(j*numGet+i));
-      }
+var createPlayBoard = function () {
+  var won=[];
+  $("div.main").remove();
+  numGet = $("input[name='games']:checked").val();
+  for (var j = 0; j < numGet; j++) {
+    var $newDivOut = $("<div class='main'></div>");
+    $newDivOut.appendTo(document.body);
+    for (var i = 0; i < numGet; i++) {
+      var $newDivIn = $("<div class='cell' id='cell"+(j*numGet+i)+"'></div>");
+      $newDivIn.appendTo($newDivOut);
+      cells.push("cell"+(j*numGet+i));
+      // won.push([i,j]);
+      // for (var k = 0; k < won.length; k++) {
+      //   for (var m = 0; m < numGet; m++) {
+      //   console.log(won[k][m]);
+      //   }
+      // }
     }
   }
   addEventHandlers();
 }
 
-// var cells = {};
-// var createPlayBoard = function (numGet) {
-//
-//   for (var j = 0; j < numGet; j++) {
-//     var $newDivOut = $("<div></div>");
-//     $newDivOut.appendTo(document.body);
-//     for (var i = 0; i < numGet; i++) {
-//       var $newDivIn = $("<div class='cell' id='cell"+(j*numGet+i)+"'></div>");
-//       $newDivIn.appendTo($newDivOut);
-//       cells["cell"+(j*numGet+i)] = [i,j];
-//     }
-//   }
-// }
-// createPlayBoard(3);
-//
-//
-// var checkWin = function(playerCurrent) {
-//   playerCurrent.map(function(location) {
-//     return location[0];
-//   });
-// }
-
 var addEventHandlers = function () {
   $("div.cell").off();
   $("div.cell").one("click", function (event) {
     var $currentCell = $(this);
-    $currentCell.css("background", "black")
+    $currentCell.css("background", "black");
+
     if (currentPlayer === playerOne) {
       $currentCell.css("backgroundImage", "url(images/cross.png)");
-      console.log($currentCell.attr('id'));
-      playerOneCurrent. push(cells.indexOf($currentCell.attr('id')));
+      playerOneCurrent.push(cells.indexOf($currentCell.attr('id')));
       currentPlayer = playerTwo;
-      playOneCurrentSorted = playerOneCurrent.sort(function(a, b){return a-b});
+      var playOneCurrentSorted = playerOneCurrent.sort(function(a, b){return a-b});
+      console.log('move:', playOneCurrentSorted);
+      console.log('totalGrid:', numGet);
       wonThree.forEach(function(win) {
-        var result = playOneCurrentSorted.join('').indexOf(win.join(''));
-        if(result != -1) {
-          $("#popupcontent").html("Congratulations! \n Player One is the winner!");
+        var numOfWins = win.filter(function(value) { return playOneCurrentSorted.indexOf(value) != -1}).length;
+        console.log('numberOfWins:', numOfWins);
+
+        if(numOfWins === 3) {
+          $("#popupcontent").html("Congratulations!  Player One is the winner!");
           showPopup(400,200);
           $("div.cell").css({
-            "pointer-events":"none",
-            // "background": "none",
-            // "backgroundImage":"none"
+            "pointer-events":"none"
           })
         }
       });
 
     } else {
       $currentCell.css("backgroundImage", "url(images/circle.png)");
-      playerTwoCurrent. push(cells.indexOf($currentCell.attr('id')));
-      console.log(playerTwoCurrent);
+      playerTwoCurrent.push(cells.indexOf($currentCell.attr('id')));
       currentPlayer = playerOne;
-      playTwoCurrentSorted = playerTwoCurrent.sort(function(a, b){return a-b});
+      var playTwoCurrentSorted = playerTwoCurrent.sort(function(a, b){return a-b});
+      console.log('move:', playTwoCurrentSorted);
       wonThree.forEach(function(win) {
-        var result = playerTwoCurrent.join('').indexOf(win.join(''));
-        if(result != -1) {
-          console.log("playerTwo won!");
-          $("div.cell").css("pointer-events","none")
+        var numOfWins = win.filter(function(value) { return playTwoCurrentSorted.indexOf(value) != -1}).length;
+        console.log('numberOfWins:', numOfWins);
+
+        if(numOfWins === 3) {
+          $("#popupcontent").html("Congratulations!  Player Two is the winner!");
+          showPopup(400,200);
+          $("div.cell").css({
+            "pointer-events":"none"
+          })
         }
       });
+
     }
+
     console.log('innerHTML: ', $currentCell.attr('id'));
   })
 }
-
-
-
 
 var baseText = null;
 
 function showPopup(w,h){
    var popUp = document.getElementById("popupcontent");
 
-   popUp.style.top = "200px";
+   popUp.style.top = "100px";
    popUp.style.width = w + "px";
    popUp.style.height = h + "px";
 
    if (baseText == null) baseText = popUp.innerHTML;
    popUp.innerHTML = baseText +
-      "<div id=\"statusbar\"><button onclick=\"hidePopup();\">Close window<button></div>";
+      "<div id=\"statusbar\"><button onclick=\"hidePopup();\">Try again<button></div>";
 
    var sbar = document.getElementById("statusbar");
-   sbar.style.marginTop = (parseInt(h)-40) + "px";
+   sbar.style.marginTop = (parseInt(h)-100) + "px";
    popUp.style.visibility = "visible";
 }
-
 
 function hidePopup(){
    var popUp = document.getElementById("popupcontent");
    popUp.style.visibility = "hidden";
+   createPlayBoard();
+   playerOneCurrent = [];
+   playerTwoCurrent = [];
+   won = [];
+   playerOne = 1;
+   playerTwo = -1;
+
 }
