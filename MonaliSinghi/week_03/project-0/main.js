475c1c09dbe8ac2eede26be7fc8b365c868c7626
Monali <monali.sac@gmail.com.com>
2016-05-12 13:00:29 +1000

    made few changes

--- MonaliSinghi/week_03/project-0/main.js
+++ MonaliSinghi/week_03/project-0/main.js
@@ -15,7 +15,7 @@ var winningCombinations = [
   [2, 4, 6]
 ];
 
-// defining current playing as X
+// defining current player as X
 var currentPlayer = 'x';
 var scoreX = 0;
 var scoreO = 0;
@@ -36,34 +36,43 @@ $('.board').on('click', ".cell:not('.cell-x, .cell-o')", function(event) {
   currentPlayerCells.push(indexOfCell);
 
   $.each(winningCombinations, function(index, combination) {
-    var hasAllCells = true;
+    var winner = true;
 
     $.each(combination, function(index, cell) {
       if ($.inArray(cell, currentPlayerCells) === -1) {
-        hasAllCells = false;
+        winner = false;
       }
     });
 
-    if (hasAllCells) {
+    if (winner) {
       if (currentPlayer === 'x'){
         scoreX++;
       }
       else{
         scoreO++;
       }
-      if (scoreX < 2)
+      if (scoreX < 2){
         $(".score-x").text("X won " + scoreX + " time");
-      else
+      }  else {
         $(".score-x").text("X won " + scoreX + " times");
-      if (scoreO < 2)
-        $(".score-o").text("O won " + scoreO + " time");
-      else
-        $(".score-o").text("O won " + scoreO + " times");
-      alert(currentPlayer + ' wins!');
-      clearBoard();
-    }
-  });
+      }
 
+      if (scoreO < 2){
+        $(".score-o").text("O won " + scoreO + " time");
+      } else {
+        $(".score-o").text("O won " + scoreO + " times");
+      }
+      swal({title: "Good job!", text: "You won!", type: "success"}, clearBoard);
+
+    }
+
+
+  // test for a Tie
+
+  if (chosenCells.x.length + chosenCells.o.length === 9) {
+    swal({title: "It's a Tie!"}, clearBoard);
+  }
+  });
   // Swap current player's token
   if (currentPlayer === 'x') {
     currentPlayer = 'o';
@@ -74,11 +83,17 @@ $('.board').on('click', ".cell:not('.cell-x, .cell-o')", function(event) {
 
 // clear game board once the game is over
 var clearBoard = function(){
-  $('.wave').slideDown("slow").slideUp("fast");
+  $('.wave').slideDown("slow");
+  var sound = $('#wave-sound')[0];
+  sound.play();
+  setTimeout(function(){
+    $(sound).animate({volume: 0}, 1000);
+  }, 4500);
+  $('.wave').slideUp("fast");
   $('.cell').removeClass('cell-x').removeClass('cell-o');
   chosenCells = {
     'x': [],
     'o': []
   };
-  currentPlayer = 'x';
+  currentPlayer = 'x'
 };
---
+++ MonaliSinghi/week_03/project-0/readme.txt
@@ -0,0 +1,4 @@
+Tic Tac token
+This Game is simple,there are two game value's X and O.
+You have to click value on game board and whoever get three X or O in a row,column or diagonally, wins.
+Than the game board resets.You can start new game. The score board keeping a track of winners.
