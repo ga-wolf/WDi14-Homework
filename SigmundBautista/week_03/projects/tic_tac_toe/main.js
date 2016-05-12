7b7696a4357184bfd7665ab47e634fdc3e096197
sigmoidium <sj-bautista@hotmail.com>
2016-05-12 17:16:24 +1000

    Night before copy fully functional

--- SigmundBautista/week_03/projects/tic_tac_toe/main.js
+++ SigmundBautista/week_03/projects/tic_tac_toe/main.js
@@ -1,23 +1,23 @@
-// remove next two lines
-var drawArray = [1, 3, 6, 8]
-var drawIndex = 0;
-
 var board = ["", "", "", "", "", "", "", "", ""];
-var turnTimer = 0; // Initial turn tracking value
+var turnTimer = 1;
+var playerTurn = "";
 var winCheck = "no win";
 var p = ""; // Player var
 var q = ""; // computer/player 2 var
 var gameOver = false;
-var gameMode = "";
+var gameMode = "vsPlayer"
 
+var something = $("#player1").val
 //set defaults
-$("#PlayerVsPlayer").on("click", function(){
-  gameMode = "vsPlayer";
+$("#pvp").change(function(){
+  if(this.checked){
+    gameMode = "vsPlayer"
+  }
 })
-
-//VS AI button
-$("#PlayerVsDumbAi").on("click", function(){
-  gameMode = "dumbass";
+$("#pva").change(function(){
+  if(this.checked){
+    gameMode = "vsDumbass"
+  }
 })
 
 //Use type X
@@ -38,6 +38,14 @@ $("#newGame").on("click", function(){
   turnTimer = 1;
   winCheck = "no win";
   gameOver = false;
+  $(".playersImg").css("visibility", "visible");
+  if(gameMode === "vsDumbass")  {
+    $("#player1Img").attr("src", "player1.gif");
+    $("#player2Img").attr("src", "dumbass.gif");
+  } else if(gameMode = "vsPlayer")  {
+    $("#player1Img").attr("src", "player1.gif");
+    $("#player2Img").attr("src", "player1.gif");
+  }
   $(".turnTimer").text("Player turn " + turnTimer);
   for(i = 0; i <= board.length; i++)  {
     $("#box" + i).text("");
@@ -47,7 +55,7 @@ $("#newGame").on("click", function(){
 
 // On clicks for AI
 $(".Box").on("click", function(){
-  if(gameMode === "dumbass")  {
+  if(gameMode === "vsDumbass")  {
     var index = $(this).attr("index")
     if(board[index] === "X" || board[index] === "O") {
       alert("Cant do that dawg")
@@ -58,22 +66,24 @@ $(".Box").on("click", function(){
     }
   } else if(gameMode === "vsPlayer")  {   // this is for player clicks
     var index = $(this).attr("index")
-    var num = ";"
     if(board[index] === "X" || board[index] === "O") {
       alert("Cant do that dawg")
     } else {
-        if(turnTimer % 2 === 0) {
+        if(turnTimer % 2 != 0) {
           board[index] = p;
+          playerTurn = "Player 1";
+          $(".playerTurn").text(playerTurn + "'s turn");
           $("#box" + index).text(p);
         } else {
           board[index] = q;
+          playerTurn = "Player 2";
+          $(".playerTurn").text(playerTurn + "'s turn");
           $("#box" + index).text(q);
         }
         //add click change text player 2
         playerVSConditionals();
     }
   }
-
 })
 
 //AI kind of..
@@ -91,19 +101,19 @@ var ai = function (){
 //create randdom number
 var randomNumber = function() {
   index = Math.floor(Math.random() * 9);
-  // return index;
-  return drawArray[drawIndex++];
+  return index;
+  // return drawArray[drawIndex++];
 };
 
 //Turn Timer
 var switchTurns = function()  {
   turnTimer++;
-  $(".turnTimer").text("Player turn " + turnTimer);
+    $(".turnTimer").text("Player turn " + turnTimer);
   return turnTimer;
 };
 
 //Player conditions for Victory
-var playerWinConditions =  function() {
+var player1WinConditions =  function() {
   if ((board[0] === p && board[1] === p && board[2] === p) ||
       (board[3] === p && board[4] === p && board[5] === p) ||
       (board[6] === p && board[7] === p && board[8] === p) ||
@@ -116,15 +126,27 @@ var playerWinConditions =  function() {
       (board[6] === p && board[4] === p && board[2] === p)
     ) {
     winCheck = "Player 1 Win";
-    alert("Winner Winner chicken dinner");
+    if(gameMode === "vsDumbass")  {
+      swal({  title: "Winner winner Chicken Dinner!",
+              text: "He's special..",
+              timer: 2000,
+              showConfirmButton: false
+            });
+    } else if(gameMode === "vsPlayer")  {
+      swal({  title: "Player 1 Winner",
+              text: "Dont feel too proud you went first...ass",
+              timer: 2000,
+              showConfirmButton: false
+            });
+    }
     gameOver = true;
     //turns is > 4 moves "how dud you manage to take you that long to beat my dumbass AI!"
 
   }
 };
 
-//AI win condtions
-var aiWinConditions =  function() {
+//AI win condtions...
+var player2WinConditions =  function() {
   if ((board[0] === q && board[1] === q && board[2] === q) ||
       (board[3] === q && board[4] === q && board[5] === q) ||
       (board[6] === q && board[7] === q && board[8] === q) ||
@@ -137,7 +159,19 @@ var aiWinConditions =  function() {
       (board[6] === q && board[4] === q && board[2] === q)
     ) {
     winCheck = "Player 2 Win";
-    alert("How the fuck did you lose this?!");
+    if(gameMode === "vsDumbass")  {
+      swal({  title: "HOW?! JUST HOW THE HELL DID YOU LOSE!!!",
+              text: "Cmoooonn he has the IQ of a bucket...",
+              timer: 3000,
+              showConfirmButton: false
+            });
+    } else if(gameMode === "vsPlayer")  {
+      swal({  title: "Player 2 Winner",
+              text: "Nice!",
+              timer: 2000,
+              showConfirmButton: false
+            });
+    }
     gameOver = true;
   }
 };
@@ -151,33 +185,29 @@ var drawConditions = function() {
       }
     }
   if(gameOver === true && winCheck === "no win")  {
-    alert("you drew bro..")
+    swal({  title: "You Drew",
+            text: "you see this alot dont you?",
+            timer: 1500,
+            showConfirmButton: false
+          });
   }
 };
 
 //Click functions for Dumbass AI Play
 var dumbassConditionals =  function() {
-  playerWinConditions();
+  player1WinConditions();
   drawConditions();
-  if(gameOver === true) {
-    alert("Game pover");
-  }  else {
-    switchTurns();
-    ai();
-    aiWinConditions();
-    drawConditions();
-  }
+  switchTurns();
+  ai();
+  player2WinConditions();
+  drawConditions();
 };
 
 //Click functions for PVP play
 var playerVSConditionals = function() {
-  playerWinConditions();
+  player1WinConditions();
   drawConditions();
-  aiWinConditions();
-  if(gameOver === true) {
-    alert("Game pover");
-  }
-  else {
-}   switchTurns();
-    drawConditions();
+  player2WinConditions();
+  switchTurns();
+  drawConditions();
 }
