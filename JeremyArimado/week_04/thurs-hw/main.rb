befebd5235a4d51f3b9d9f9a2f363124c15cfdda
Jeremy Arimado <jarimado@gmail.com>
2016-05-19 20:33:13 +1000

    thurs hw complete

--- JeremyArimado/week_04/thurs-hw/main.rb
+++ JeremyArimado/week_04/thurs-hw/main.rb
@@ -4,7 +4,14 @@ require 'sinatra/reloader'
 require "sqlite3"
 @dbase = "database.db"
 
+def commandDb (sqlCommand)
+    db = SQLite3::Database.new "database.db";
+    db.results_as_hash = true;
+    return db.execute(sqlCommand);
+end
+
 get '/' do
+    @chans = commandDb("SELECT * FROM chans;");
     erb :home
 end
 
@@ -13,20 +20,43 @@ get '/new' do
 end
 
 post '/new' do
-
     title = params[:title];
     image = params[:image];
     content = params[:content];
-
-    db = SQLlite3::Database.new "database.db"
-    db.results_as_hash = true
-
-    binding.pry
-
-    sql = "INSERT INTO chans (title, image, content)"
-    sql << " VALUES ('#{title}', '#{image}', '#{content}');"
-
-    db.execute sql
-
+    sql = "INSERT INTO chans (title, image, content)";
+    sql << " VALUES ('#{title}', '#{image}', '#{content}');";
+    commandDb(sql);
     redirect "/"
+end
+
+get '/chan/:id' do
+    current_id = params[:id];
+    sql = "SELECT * FROM chans WHERE id == #{current_id};";
+    @chan = commandDb(sql)[0];
+    erb :chan;
+end
+
+
+get '/chan/:id/edit' do
+    current_id = params[:id];
+    sql = "SELECT * FROM chans WHERE id == #{current_id};";
+    @chan = commandDb(sql)[0];
+    erb :edit;
+end
+
+post '/chan/:id' do
+    current_id = params[:id];
+    title = params[:title];
+    image = params[:image];
+    content = params[:content]
+    sql = "UPDATE chans SET title = '#{title}', image = '#{image}', content = '#{content}' WHERE id = #{current_id};";
+    commandDb(sql);
+    redirect "/chan/#{current_id}"
+end
+
+get '/chan/:id/delete' do
+    current_id = params[:id];
+    sql = "DELETE FROM chans WHERE id == #{current_id};";
+    @chan = commandDb(sql);
+    redirect '/';
 end
