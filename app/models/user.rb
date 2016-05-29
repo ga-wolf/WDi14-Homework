ae3fb47994774935bb3440b54f2cd5c04ac7f679
yumimac <yumi.ko.cheers@gmail.com>
2016-05-29 21:31:28 +1000

    Reset migration and redo annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,13 @@
 # Table name: users
 #
 #  id              :integer          not null, primary key
-#  name            :text
-#  email           :text
+#  name            :text             not null
+#  email           :text             not null
 #  password_digest :text
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #
 
 class User < ActiveRecord::Base
+  has_secure_password
 end
