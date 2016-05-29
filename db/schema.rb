4bb2fda10af9beeb9f1ac2806f7ab1892569229f
yumimac <yumi.ko.cheers@gmail.com>
2016-05-30 03:09:36 +1000

    Migrate avatar column to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160529025044) do
+ActiveRecord::Schema.define(version: 20160529170618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160529025044) do
     t.integer  "region_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "avatar"
   end
 
   create_table "spots", force: :cascade do |t|
