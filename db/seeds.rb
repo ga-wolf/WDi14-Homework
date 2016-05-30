2c48d59398589d0fb2d42a25be7c450e5b06c7e5
yumimac <yumi.ko.cheers@gmail.com>
2016-05-30 22:37:32 +1000

    Modify spot seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,4 @@ Region.destroy_all
 r1 = Region.create :name => 'Central Coast', :address => 'central coast', :description => 'All the top angling species can be taken from the lakes and along the surfing beaches.'
 
 Spot.destroy_all
-s1 = Spot.create :name => 'The Skillion', :address => 'Terrigal NSW 2260', :description => 'The Skilion is a great spot for lures and live bait. Kingfish, tuna and snapper are taken here, as well as tailor during the season', :region_id => r1.id 
+s1 = Spot.create :name => 'The Skillion', :address => 'Terrigal NSW 2260', :description => 'The Skilion is a great spot for lures and live bait. Kingfish, tuna and snapper are taken here, as well as tailor during the season', :region_id => r1.id
