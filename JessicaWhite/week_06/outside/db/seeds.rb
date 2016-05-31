1c8da9b61220c7b8627909cf926b8353e99d3874
more-like-a-puma <jessicamaycreative@gmail.com>
2016-05-31 16:24:01 +1000

    lol

---
+++ JessicaWhite/week_06/outside/db/seeds.rb
@@ -0,0 +1,2 @@
+User.destroy_all
+morelikeapuma = User.create :username => 'more_like_a_puma', :email => 'jwannabel@gmail.com', :name => 'Jessica May', :password => 'chicken', :password_confirmation => 'chicken'
