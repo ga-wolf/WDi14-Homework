f706cfee549ee3f671ae877cb24cc6d9af90fd88
yumimac <yumi.ko.cheers@gmail.com>
2016-06-01 12:25:07 +1000

    Remove avatar column from reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160531130624) do
+ActiveRecord::Schema.define(version: 20160601022323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20160531130624) do
     t.integer  "region_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "avatar"
     t.text     "link"
   end
 
