24a19dd6b26e2f259a19f341e3a2166e9e87a61f
fbi1714 <fcamatti@ucs.br>
2016-06-09 19:15:22 +1000

    Saving Underscore JS homework

--- FabioCamatti/week_07/11_backbone/underscore/fifth_exercises/js/underscore_fifth_exercises.js
+++ FabioCamatti/week_07/11_backbone/underscore/fifth_exercises/js/underscore_fifth_exercises.js
@@ -1,35 +1,100 @@
+$(document).ready(function() {
+    var people = [{
+        id: 1,
+        username: "A",
+        active: true,
+        age: 20,
+        uid: 1412
+    }, {
+        id: 2,
+        username: "B",
+        active: false,
+        age: 35,
+        uid: 1352
+    }, {
+        id: 3,
+        username: "C",
+        active: false,
+        age: 50,
+        uid: 5418
+    }, {
+        id: 4,
+        username: "D",
+        active: true,
+        age: 65,
+        uid: 216
+    }, {
+        id: 5,
+        username: "E",
+        active: true,
+        age: 80,
+        uid: 18
+    }, {
+        id: 6,
+        username: "E",
+        active: true,
+        age: 95,
+        uid: 1000
+    }];
 
-var people = [
-  { id: 1, username: "A", active: true,  age: 20, uid: 1412 },
-  { id: 2, username: "B", active: false, age: 35, uid: 1352 },
-  { id: 3, username: "C", active: false, age: 50, uid: 5418 },
-  { id: 4, username: "D", active: true,  age: 65, uid: 216  },
-  { id: 5, username: "E", active: true,  age: 80, uid: 18   },
-  { id: 6, username: "E", active: true,  age: 95, uid: 1000 }
-];
+    var startingData = [
+        [
+            ["groucho", "marx", 1990],
+            ["firstName", "lastName", "born"]
+        ],
+        [
+            ["chico", "marx", 1987],
+            ["firstName", "lastName", "born"]
+        ],
+        [
+            ["zeppo", "marx", 2001],
+            ["firstName", "lastName", "born"]
+        ],
+        [
+            ["harpo", "marx", 1988],
+            ["firstName", "lastName", "born"]
+        ],
+        [
+            ["gummo", "marx", 1992],
+            ["firstName", "lastName", "born"]
+        ]
+    ];
 
-var startingData = [
-  [["groucho", "marx", 1990], ["firstName", "lastName", "born"]],
-  [["chico",   "marx", 1987], ["firstName", "lastName", "born"]],
-  [["zeppo",   "marx", 2001], ["firstName", "lastName", "born"]],
-  [["harpo",   "marx", 1988], ["firstName", "lastName", "born"]],
-  [["gummo",   "marx", 1992], ["firstName", "lastName", "born"]]
-];
-
-// Log out 30 random numbers between 20 and 100
-_(30).times(function(n){ return _.random(20,100); });
+    // Log out 30 random numbers between 20 and 100
+    _(30).times(function(n) {
+        return _.random(20, 100);
+    });
 
 
-// Create a function that can only ever be called once
-var sayFabio = function () {
-  console.log("Fabio");
-};
+    // Create a function that can only ever be called once
+    var sayFabio = function() {
+        console.log("Fabio");
+    };
 
-var initialize = _.once(sayFabio);
-initialize();
-initialize();
+    var initialize = _.once(sayFabio);
+    initialize();
+    initialize();
 
-// Append a paragraph tag to the body for every person in people. It should end up looking something like this - <p> Hello A, you don't look a day over 20 </p>
-var peopleTemplate = _.template("<p> Hello <%= name %>, you don't look a day over 20 </p>");
+    // Append a paragraph tag to the body for every person in people. It should end up looking something like this - <p> Hello A, you don't look a day over 20 </p>
+    var compiled = _.template("hello: <%= name %>");
+    compiled({
+        name: 'moe'
+    });
 
-var doIt = _.chain(people).peopleTemplate();
+    var peopleTemplate = _.template("<p> Hello <%= username %>, you don't look a day over <%= age %> </p>");
+    var insertPeopleTemplate = _.chain(people).map(function(p) {
+        return peopleTemplate({
+            username: p.username,
+            age: p.age
+        });
+    }).value();
+
+    var peopleString = "";
+
+    jQuery.each(insertPeopleTemplate, function(i, val) {
+        peopleString += val;
+    });
+
+    $(peopleString).insertAfter("h1");
+
+});
