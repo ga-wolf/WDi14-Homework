be1e99c9785f7ceeb6af205a6876828020c2d900
fbi1714 <fcamatti@ucs.br>
2016-06-09 20:41:33 +1000

    Today's homework finish. Let's see tomorrow!

--- FabioCamatti/week_07/11_backbone/underscore/fifth_exercises/js/underscore_fifth_exercises.js
+++ FabioCamatti/week_07/11_backbone/underscore/fifth_exercises/js/underscore_fifth_exercises.js
@@ -37,6 +37,39 @@ $(document).ready(function() {
         uid: 1000
     }];
 
+    // Log out 30 random numbers between 20 and 100
+    _(30).times(function(n) {
+        return _.random(20, 100);
+    });
+
+
+    // Create a function that can only ever be called once
+    var sayFabio = function() {
+        console.log("Fabio");
+    };
+
+    var initialize = _.once(sayFabio);
+    initialize();
+    initialize();
+
+    // Append a paragraph tag to the body for every person in people. It should end up looking something like this - <p> Hello A, you don't look a day over 20 </p>
+
+    var peopleTemplate = _.template("<p> Hello <%= username %>, you don't look a day over <%= age %> </p>");
+    var insertPeopleTemplate = _.chain(people).map(function(p) {
+        return peopleTemplate({
+            username: p.username,
+            age: p.age
+        });
+    }).value();
+
+    var peopleString = "";
+
+    jQuery.each(insertPeopleTemplate, function(i, val) {
+        peopleString += val;
+    });
+
+    $(peopleString).insertAfter("h1");
+
     var startingData = [
         [
             ["groucho", "marx", 1990],
@@ -60,41 +93,38 @@ $(document).ready(function() {
         ]
     ];
 
-    // Log out 30 random numbers between 20 and 100
-    _(30).times(function(n) {
-        return _.random(20, 100);
+    var pessoasObject = _.chain(startingData)
+        .map(function(person) {
+            return _.invert(_.object(_.unzip(person)));
+        })
+        .map(function(p) {
+            return _.mapObject(p, function(val, key) {
+                if (key === "born") {
+                    return parseInt(val) - 100;
+                } else {
+                    return val[0].toUpperCase() + _.rest(val).join("");
+                }
+            });
+        })
+        .value();
+
+    var pessoasTemplate = _.template("<p><%= firstName %> <%= lastName %> was born in <%= born %></p>");
+
+    var insertPessoasTemplate = _.chain(pessoasObject)
+        .map(function(p) {
+            return pessoasTemplate({
+                firstName: p.firstName,
+                lastName: p.lastName,
+                born: p.born
+            });
+        }).value();
+
+    var pessoasString = "";
+    jQuery.each(insertPessoasTemplate, function(i, val) {
+        pessoasString += val;
     });
 
-
-    // Create a function that can only ever be called once
-    var sayFabio = function() {
-        console.log("Fabio");
-    };
-
-    var initialize = _.once(sayFabio);
-    initialize();
-    initialize();
-
-    // Append a paragraph tag to the body for every person in people. It should end up looking something like this - <p> Hello A, you don't look a day over 20 </p>
-    var compiled = _.template("hello: <%= name %>");
-    compiled({
-        name: 'moe'
-    });
-
-    var peopleTemplate = _.template("<p> Hello <%= username %>, you don't look a day over <%= age %> </p>");
-    var insertPeopleTemplate = _.chain(people).map(function(p) {
-        return peopleTemplate({
-            username: p.username,
-            age: p.age
-        });
-    }).value();
-
-    var peopleString = "";
-
-    jQuery.each(insertPeopleTemplate, function(i, val) {
-        peopleString += val;
-    });
-
-    $(peopleString).insertAfter("h1");
+    $("p").last().append(pessoasString);
+      // $(pessoasString).insertAfter("h1");
 
 });
