5d5a992658240448bfffc900d81ef35bfbe719d8
fbi1714 <fcamatti@ucs.br>
2016-06-10 12:55:34 +1000

    Saving today class material

--- FabioCamatti/week_07/11_backbone/backbone_zoo/js/main.js
+++ FabioCamatti/week_07/11_backbone/backbone_zoo/js/main.js
@@ -8,14 +8,14 @@ var Movie = Backbone.Model.extend({
     },
     initialize: function () {
       this.on("change", function () {
-        console.log( "Something changed" );
+        // console.log( "Something changed" );
       });
       this.on("change:name", function () {
         var changedAttributes = this.previousAttributes();
         var currentAttributes = this.toJSON();
 
-        console.log( "Name was changed from: " + changedAttributes.name); console.log( "Name was changed to: " + this.get("name") );
-        
+        // console.log( "Name was changed from: " + changedAttributes.name); console.log( "Name was changed to: " + this.get("name") );
+
       });
     }
 });
@@ -25,13 +25,13 @@ var newMovie = new Movie({
     duration: 450,
     director: "Bela Tarr"
 });
-console.log(newMovie.toJSON());
+// console.log(newMovie.toJSON());
 
 var genericMovie = new Movie();
 var specificMovie = new Movie({
     name: "Satan"
 });
-console.log(genericMovie.toJSON(), specificMovie.toJSON());
+// console.log(genericMovie.toJSON(), specificMovie.toJSON());
 
 var movieName = newMovie.get("name");
 
@@ -46,12 +46,59 @@ anotherMovie.set({
     madeBy: "Pixar"
 });
 
-console.log(anotherMovie.toJSON());
+// console.log(anotherMovie.toJSON());
 
 var Animal = Backbone.Model.extend({
-    initialize: function() {
-        console.log("New animal created");
+    // initialize: function() {
+    //     console.log("New animal created");
+    // }
+
+    defaults: {
+      type: 'animal',
+      ecosystem: "",
+      stripes: 0,
+      living: true
+    },
+    initialize: function () {
+      this.on("change", function () {
+        console.log("Something just changed");
+      });
+
+      this.on("changed:type", function () {
+        console.log("The type just changed");
+      });
+
     }
 });
 
-var newAnimal = new Animal();
+// var newAnimal = new Animal();
+//
+// console.log(newAnimal.toJSON());
+
+var kangaroo = new Animal();
+var living = kangaroo.get("living");
+// console.log( living );
+
+kangaroo.set("type", "kangaroo");
+
+kangaroo.set({
+  pouch: true,
+  tail: true
+});
+
+var kangarooAttrs = kangaroo.toJSON();
+// console.log( kangarooAttrs );
+
+var butterfly = new Animal({
+  type: "Badger"
+});
+
+var panda = new Animal({
+  type: "Panda",
+  living: false,
+  id: 10
+});
+
+var fabio = function () {
+
+};
