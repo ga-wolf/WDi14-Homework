fb6432b55f8d1975b8bbb10407693cfb35b1284d
fbi1714 <fcamatti@ucs.br>
2016-06-29 17:25:30 +1000

    .

---
+++ FabioCamatti/week_10/homework/29th-June-2016/js/task_1.js
@@ -0,0 +1,27 @@
+var color_from_hue = function(hue) {
+        var h = hue / 60;
+        var c = 255;
+        var x = (1 - Math.abs(h % 2 - 1)) * 255;
+        var color;
+
+        var i = Math.floor(h);
+        if (i === 0) color = rgb_to_hex(c, x, 0);
+        else if (i == 1) color = rgb_to_hex(x, c, 0);
+        else if (i == 2) color = rgb_to_hex(0, c, x);
+        else if (i == 3) color = rgb_to_hex(0, x, c);
+        else if (i == 4) color = rgb_to_hex(x, 0, c);
+        else color = rgb_to_hex(c, 0, x);
+
+        return color;
+    };
+
+  var rgb_to_hex = function(red, green, blue) {
+        var h = ((red << 16) | (green << 8) | (blue)).toString(16);
+        // add the beginning zeros
+
+        while (h.length < 6) h = '0' + h;
+        return '#' + h;
+    };
+$(document).ready(function () {
+  $(".main").css({backgroundColor: "#ffe", borderLeft: "5px solid #ccc" })
+});
